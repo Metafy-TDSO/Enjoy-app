203f896acb7af8051985cc3c84fa771158efbe18
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2022-09-10 02:04:08 -0300

    feat: make event details screen a modal

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,16 +1,27 @@
 import { createStackNavigator } from '@react-navigation/stack'
 
-import { HomeScreen, EventDetails, SearchScreen, NavigationScreen } from '~screens'
+import { HomeScreen, EventDetails, SearchScreen } from '~screens'
 
 const { Navigator, Screen } = createStackNavigator<RootStackParamList>()
 
 export const RootNavigator = (): JSX.Element => {
   return (
-    <Navigator screenOptions={{ headerShown: false, animationEnabled: true }}>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        animationEnabled: true,
+        cardShadowEnabled: true
+      }}
+    >
       <Screen name="Home" component={HomeScreen} />
-      <Screen name="EventDetails" component={EventDetails} />
+      <Screen
+        name="EventDetails"
+        options={{
+          presentation: 'modal'
+        }}
+        component={EventDetails}
+      />
       <Screen name="Search" component={SearchScreen} />
-      <Screen name="Navigation" component={NavigationScreen} />
     </Navigator>
   )
 }
