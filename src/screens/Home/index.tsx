b4206fdceb5c70fff771adc3fe74c5b651af63a4
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2022-09-10 02:04:59 -0300

    feat: add home screen with styled google map

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,30 +1,171 @@
-import { useNavigation } from '@react-navigation/native'
-import { Button, Center, Text, Image } from 'native-base'
+import { Center, Container, Input, Spinner } from 'native-base'
+import { useEffect, useRef, useState } from 'react'
+import { Dimensions, StyleSheet, Alert } from 'react-native'
+import MapView, { Region, PROVIDER_GOOGLE, Marker } from 'react-native-maps'
+import { getCurrentPositionAsync, requestForegroundPermissionsAsync } from 'expo-location'
+import { useQuery } from 'react-query'
 
-export const HomeScreen = (): JSX.Element => {
-  const navigation = useNavigation()
+import { commonColors, fontSizes, GOOGLE_MAPS_APIKEY } from '~constants'
+import mapStyle from './map.style.json'
+import MapViewDirections from 'react-native-maps-directions'
+import { websocketClient } from '~services/client'
+import { Event } from '~services/models'
+import { getEventById } from '~services/api'
+import { BlurView } from 'expo-blur'
+import { DirectionsTab } from '~components/NavigationTab'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+const deltas = {
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421
+}
+
+export type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>
+
+export const HomeScreen = ({ route, navigation }: HomeScreenProps): JSX.Element => {
+  const eventId = route.params?.eventId
+
+  const [position, setPosition] = useState<Region>({ latitude: 0, longitude: 0, ...deltas })
+  const { data: navigationData, isLoading } = useQuery(['navigation event'], () =>
+    eventId ? getEventById(eventId) : null
+  )
+  const [nearEvents, setNearEvents] = useState<Event[]>()
+  const mapEl = useRef<MapView>(null)
+  const [initialDistance, setInitialDistance] = useState<number>()
+  const [navigationState, setNavigationState] = useState<{ distance: number; duration: number }>()
+  const [isNavigating, setIsNavigating] = useState<boolean>(Boolean(eventId))
+
+  useEffect(() => {
+    async function loadPosition() {
+      const { status } = await requestForegroundPermissionsAsync()
+
+      if (status !== 'granted') {
+        Alert.alert('Ops!', 'É necessário autorizar o uso do GPS para obter sua localização')
+        return
+      }
+
+      const currentPosition = await getCurrentPositionAsync()
+
+      const { latitude, longitude } = currentPosition.coords
+
+      setPosition({ latitude, longitude, ...deltas })
+    }
+
+    loadPosition()
+  }, [])
+
+  useEffect(() => {
+    websocketClient.on('server:event:search-near', (events: Event[]) => {
+      setNearEvents(events)
+    })
+
+    return () => {
+      websocketClient.off('server:event:search-near')
+    }
+  }, [])
+
+  useEffect(() => {
+    setInterval(() => {
+      if (position.latitude > 0 && position.longitude > 0) {
+        websocketClient.emit('client:event:search-near', position)
+      }
+    }, 2000)
+  }, [])
+
+  const handleCancelNavigation = () => {
+    setIsNavigating(false)
+    setInitialDistance(undefined)
+    setNavigationState(undefined)
+  }
 
   return (
-    <Center flex={1}>
-      <Image
-        source={require('~assets/logo.png')}
-        resizeMode="contain"
-        resizeMethod="resize"
-        height={24}
-        alt="logo"
-      />
-      <Text textAlign="center">Hello Test2</Text>
-      <Text textAlign="center">Event info</Text>
-      <Button mt={4}>Home</Button>
-      <Button mt={4} onPress={() => navigation.navigate('Search')}>
-        Search
-      </Button>
-      <Button mt={4} onPress={() => navigation.navigate('EventDetails', { id: 'batata' })}>
-        EventDetails
-      </Button>
-      <Button mt={4} onPress={() => navigation.navigate('Navigation')}>
-        Navigation
-      </Button>
+    <Center>
+      {isLoading && (
+        <BlurView intensity={80} tint="dark" style={styles.blurContainer}>
+          <Spinner color={commonColors.primary[700]} />
+        </BlurView>
+      )}
+      <Container style={styles.searchContainer}>
+        <Input
+          placeholder="Ex: festa sleepover, pandora, arena..."
+          onPressIn={() => navigation.navigate('Search', { currentLocation: position })}
+          fontSize={fontSizes.xs}
+          style={styles.searchBar}
+        />
+      </Container>
+      <MapView
+        style={styles.map}
+        initialRegion={position}
+        provider={PROVIDER_GOOGLE}
+        customMapStyle={mapStyle}
+        ref={mapEl}
+      >
+        {isNavigating && navigationData?.event && (
+          <MapViewDirections
+            origin={position}
+            destination={{
+              latitude: navigationData.event.latitude,
+              longitude: navigationData.event.longitude
+            }}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={3}
+            strokeColor={commonColors.secondary[600]}
+            language="pt-br"
+            onReady={result => {
+              mapEl.current?.fitToCoordinates(result.coordinates, {
+                edgePadding: { top: 50, bottom: 50, left: 50, right: 50 }
+              })
+              setInitialDistance(result.distance)
+              // distance = kms, duration = minutes
+              setNavigationState({ distance: result.distance, duration: result.duration })
+            }}
+          />
+        )}
+        {!isNavigating &&
+          nearEvents?.map(event => (
+            <Marker
+              key={event.id}
+              coordinate={{
+                latitude: event.latitude,
+                longitude: event.longitude
+              }}
+              title={event.name}
+              description={event.description}
+              onPress={() => navigation.navigate('EventDetails', { event })}
+            />
+          ))}
+      </MapView>
+      {isNavigating && !navigationData?.event && navigationState && initialDistance && (
+        <DirectionsTab
+          initialDistance={initialDistance}
+          {...navigationState}
+          onCancel={handleCancelNavigation}
+        />
+      )}
     </Center>
   )
 }
+
+const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
+  searchContainer: {
+    position: 'absolute',
+    left: 40,
+    top: 32,
+    zIndex: 5000,
+    elevation: 1,
+    minWidth: Dimensions.get('window').width - 80
+  },
+  searchBar: {
+    borderRadius: 16,
+    fontWeight: 'bold'
+  },
+  blurContainer: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center'
+  }
+})
