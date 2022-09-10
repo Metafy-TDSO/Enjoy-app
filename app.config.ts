b937272294168ab69530e8ab36ade6e22de25997
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2022-09-10 01:59:03 -0300

    chore: add app configs

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,43 @@
 import { ExpoConfig, ConfigContext } from '@expo/config'
 
+const appName = 'Enjoy'
+const versionBuild = '0.0.1'
+
+function convertToAndroidVersion(version: string): number {
+  const splittedVersion = version.split('.')
+
+  return Number(`${splittedVersion.splice(0, 1)}.${splittedVersion.join('')}`)
+}
+
 export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
   return {
     ...config,
     extra: {
-      universalLinks: [],
+      universalLinks: []
     },
+    name: appName,
+    slug: appName,
+    version: versionBuild,
+    orientation: 'portrait',
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      bundleIdentifier: 'com.metafy.enjoy',
+      buildNumber: versionBuild,
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_API_KEY
+      }
+    },
+    android: {
+      package: 'com.metafy.enjoy',
+      versionCode: convertToAndroidVersion(versionBuild),
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_API_KEY
+        }
+      }
+    }
   }
 }
