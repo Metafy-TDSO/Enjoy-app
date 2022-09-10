63cba3044089c12ca9e5e20fa9412111fd52fb1a
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2022-09-10 02:04:30 -0300

    feat: add event details screen

--- src/screens/EventDetails/index.tsx
+++ src/screens/EventDetails/index.tsx
@@ -1,30 +1,78 @@
-import { useNavigation } from '@react-navigation/native'
-import { Button, Center, Text, Image } from 'native-base'
+import { MaterialIcons } from '@expo/vector-icons'
+import { Avatar, Button, Container, Heading, HStack, Icon, ScrollView, Text, VStack } from 'native-base'
+import { StyleSheet, Dimensions } from 'react-native'
+import { getDay, format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
-export const EventDetails = (): JSX.Element => {
-  const navigation = useNavigation()
+import { fontSizes, space } from '~constants'
+import { CloseButton } from '~components/CloseButton'
+
+export type EventDetailsProps = NativeStackScreenProps<RootStackParamList, 'EventDetails'>
+
+export const EventDetails = ({ navigation, route }: EventDetailsProps) => {
+  const { event } = route.params
+
+  const handleClose = () => {
+    navigation.goBack()
+  }
+
+  const handleGoToEvent = () => {
+    navigation.navigate('Home', { eventId: event.id })
+  }
 
   return (
-    <Center flex={1}>
-      <Image
-        source={require('~assets/logo.png')}
-        resizeMode="contain"
-        resizeMethod="resize"
-        height={24}
-        alt="logo"
-      />
-      <Text textAlign="center">Hello Test2</Text>
-      <Text textAlign="center">Event info</Text>
-      <Button mt={4}>EventDetails</Button>
-      <Button mt={4} onPress={() => navigation.navigate('Home')}>
-        Home
-      </Button>
-      <Button mt={4} onPress={() => navigation.navigate('Search')}>
-        EventDetails
-      </Button>
-      <Button mt={4} onPress={() => navigation.navigate('Navigation')}>
-        Navigation
-      </Button>
-    </Center>
+    <Container>
+      <HStack alignItems="space-between" justifyContent="center" style={styles.stickyHeader}>
+        <CloseButton onClose={handleClose} />
+        <Avatar size="64px" _dark={{ backgroundColor: 'black', borderColor: 'primary.600' }}>
+          <VStack>
+            <Text fontWeight={800} fontSize={fontSizes.xl}>
+              {getDay(new Date(event.startAt))}
+            </Text>
+            <Text fontSize={fontSizes.sm}>{format(new Date(event.startAt), 'MMM', { locale: ptBR })}</Text>
+          </VStack>
+        </Avatar>
+      </HStack>
+      <ScrollView>
+        <VStack space={space[40]}>
+          <Heading>{event.name}</Heading>
+
+          <HStack justifyContent="space-between">
+            <HStack>
+              <Icon as={MaterialIcons} name="location-on" size="sm" />
+              <Text>{event.address}</Text>
+            </HStack>
+            <HStack>
+              <Icon as={MaterialIcons} name="access-time" size="sm" />
+              <Text>{event.address}</Text>
+            </HStack>
+          </HStack>
+
+          <Button size="lg" variant="solid" onPress={() => handleGoToEvent()}>
+            BORA!
+          </Button>
+
+          <VStack space={space[16]}>
+            <Heading>Descrição</Heading>
+            <Text fontSize={fontSizes.xs}>{event.description}</Text>
+          </VStack>
+        </VStack>
+      </ScrollView>
+    </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  stickyHeader: {
+    position: 'absolute',
+    top: 48,
+    width: Dimensions.get('window').width - 32,
+    left: 16
+  },
+  goButton: {
+    width: '100%',
+    fontWeight: 'bold',
+    height: 48
+  }
+})
