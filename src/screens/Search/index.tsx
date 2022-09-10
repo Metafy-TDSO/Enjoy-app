f5db5b5bd858bf8fc23fa5eef3825fd6d4da877c
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2022-09-10 02:05:30 -0300

    feat: add search screen

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,28 +1,122 @@
-import { useNavigation } from '@react-navigation/native'
-import { Button, Center, Text, Image } from 'native-base'
+import { useState, useEffect, useCallback } from 'react'
+import { StyleSheet, Dimensions } from 'react-native'
+import { Container, Box, FlatList, Divider, Button, Center, Spinner } from 'native-base'
+import { useAsyncStorage } from '@react-native-async-storage/async-storage'
+import { useQuery } from 'react-query'
+import debounce from 'debounce'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
-export const SearchScreen = (): JSX.Element => {
-  const navigation = useNavigation()
+import { Result } from '~components/Result'
+import { ASYNC_STORAGE_KEYS, commonColors, space } from '~constants'
+import { SearchBar } from '~components/SearchBar'
+import { getManyEvents } from '~services/api'
+import { Event } from '~services/models'
+
+export type SearchScreenProps = NativeStackScreenProps<RootStackParamList, 'Search'>
+
+export const SearchScreen = ({ navigation, route }: SearchScreenProps) => {
+  const { currentLocation } = route.params
+
+  const [search, setSearch] = useState<string>('')
+  const { getItem: getRecentSearchesStorage, setItem: setRecentSearchesStorage } = useAsyncStorage(
+    ASYNC_STORAGE_KEYS.RECENT_SEARCHES
+  )
+  const [recentSearches, setRecentSearches] = useState<string[]>([])
+  const { data, isLoading, refetch } = useQuery(['searchResults'], () =>
+    getManyEvents({ ...currentLocation, name: search })
+  )
+
+  useEffect(() => {
+    getRecentSearchesStorage((_error, previousRecentSearches) => {
+      setRecentSearches(JSON.parse(previousRecentSearches ?? '[]'))
+    })
+  }, [])
+
+  const addRecentSearch = useCallback(async () => {
+    if (search.length < 3) return
+
+    const previousRecentSearches = await getRecentSearchesStorage()
+    const result = Array.from(new Set([search, ...JSON.parse(previousRecentSearches ?? '[]')])).slice(0, 3)
+
+    setRecentSearchesStorage(JSON.stringify(result))
+    setRecentSearches(result)
+  }, [search, setRecentSearches, setRecentSearchesStorage, getRecentSearchesStorage])
+
+  const handleGoBack = () => {
+    navigation.navigate('Home')
+  }
+
+  const handlePressResult = async (event: Event) => {
+    await addRecentSearch()
+    navigation.navigate('EventDetails', { event })
+  }
+
+  const filteredDispatch = (): void => {
+    refetch()
+  }
+  const debouncedSearch = useCallback(debounce(filteredDispatch, 300), [])
+
+  const updateSearch = (value: string): void => {
+    setSearch(value)
+    debouncedSearch()
+  }
 
   return (
-    <Center flex={1}>
-      <Image
-        source={require('~assets/logo.png')}
-        resizeMode="contain"
-        resizeMethod="resize"
-        height={24}
-        alt="logo"
-      />
-      <Text textAlign="center">Search</Text>
-      <Button mt={4} onPress={() => navigation.navigate('Home')}>
-        Home
-      </Button>
-      <Button mt={4} onPress={() => navigation.navigate('EventDetails', { id: 'batata' })}>
-        EventDetails
-      </Button>
-      <Button mt={4} onPress={() => navigation.navigate('Navigation')}>
-        Navigation
-      </Button>
-    </Center>
+    <Container paddingX={4} paddingTop={12}>
+      <Box style={styles.searchContainer}>
+        <SearchBar onGoBack={handleGoBack} value={search} handleChangeText={updateSearch} />
+      </Box>
+      {isLoading ? (
+        <Center>
+          <Spinner size="lg" />
+        </Center>
+      ) : search !== '' && data ? (
+        <FlatList
+          style={styles.resultList}
+          data={data.events}
+          ItemSeparatorComponent={() => <Divider marginBottom={space[0.5]} />}
+          renderItem={({ item }) => (
+            <Result
+              currentPosition={currentLocation}
+              event={item}
+              search={search}
+              onPress={id => {
+                const pressedEvent = data.events.find(event => event.id === id)
+                if (pressedEvent) handlePressResult(pressedEvent)
+              }}
+            />
+          )}
+          keyExtractor={item => item.id.toString()}
+        />
+      ) : (
+        <Box style={styles.resultList}>
+          <Button variant="ghost" onPress={async () => addRecentSearch()}>
+            <FlatList
+              style={styles.resultList}
+              data={recentSearches}
+              ItemSeparatorComponent={() => <Divider marginBottom={space[0.5]} />}
+              renderItem={({ item, index: key }) => (
+                <Result onPress={() => setSearch(item)} event={{ name: item, id: key }} search={search} />
+              )}
+            />
+          </Button>
+          <Divider marginTop={space[0.5]} />
+        </Box>
+      )}
+    </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    width: Dimensions.get('window').width - 36,
+    backgroundColor: commonColors.black,
+    borderWidth: 1,
+    borderColor: commonColors.secondary[600],
+    borderRadius: 8,
+    marginBottom: space[4]
+  },
+  resultList: {
+    width: Dimensions.get('window').width - 36
+  }
+})
