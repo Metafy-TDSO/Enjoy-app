5f432c6fff7e1da2a053e972807c07faa89feb36
Guilherme Vieira <guivpw68@gmail.com>
2022-09-12 18:08:40 -0300

    feat: improve close button

--- src/components/CloseButton/index.tsx
+++ src/components/CloseButton/index.tsx
@@ -1,6 +1,8 @@
 import { MaterialIcons } from '@expo/vector-icons'
 import { Avatar, IAvatarProps, IconButton, IIconButtonProps } from 'native-base'
 
+import { commonColors, fontSizes } from '~constants'
+
 export interface CloseButtonProps {
   onClose: () => void
   avatarProps?: IAvatarProps
@@ -9,12 +11,14 @@ export interface CloseButtonProps {
 
 export const CloseButton = ({ onClose, avatarProps, buttonProps }: CloseButtonProps) => {
   return (
-    <Avatar size="44px" _dark={{ backgroundColor: 'black' }} {...avatarProps}>
+    <Avatar
+      size="44px"
+      _dark={{ backgroundColor: 'black', color: commonColors.secondary[500] }}
+      {...avatarProps}
+    >
       <IconButton
+        icon={<MaterialIcons size={fontSizes['xl']} name="close" color={commonColors.secondary[500]} />}
         onPress={() => onClose()}
-        color="secondary.600"
-        as={MaterialIcons}
-        name="close"
         {...buttonProps}
       />
     </Avatar>
