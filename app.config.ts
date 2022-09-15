fb155392452191e14c7a47dc391e1bae3c3dda42
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2022-09-15 17:51:20 -0300

    feat: add eas projectid

--- app.config.ts
+++ app.config.ts
@@ -2,12 +2,16 @@ import { ExpoConfig, ConfigContext } from '@expo/config'
 
 const appName = 'Enjoy'
 const versionBuild = '0.1.0'
+const projectId = 'c47b3ce9-dc3b-4d8c-8170-2af13edf6eef'
 
 export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
   return {
     ...config,
     extra: {
-      universalLinks: []
+      universalLinks: [],
+      eas: {
+        projectId
+      }
     },
     scheme: 'enjoy',
     name: appName,
@@ -20,13 +24,11 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
     assetBundlePatterns: ['**/*'],
     ios: {
       bundleIdentifier: 'com.metafy.enjoy',
-      buildNumber: versionBuild,
       config: { googleMapsApiKey: 'AIzaSyCJr5eb7ScccyD3PY0_1ApgtAtdl_Wu6OY' },
       supportsTablet: true
     },
     android: {
       package: 'com.metafy.enjoy',
-      versionCode: 1,
       adaptiveIcon: {
         foregroundImage: './assets/icon.png',
         backgroundColor: '#0D0A14'
